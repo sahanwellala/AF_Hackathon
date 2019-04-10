418d991e67d1536b7981562c3f66396d8c69f4b8
sahanwellala <sahan1996@gmail.com>
2019-04-10 12:17:06 +0530

    Updates

---
+++ precache-manifest.183545f9d57bfb14c8ee2fa90906ef80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AF_Hackathon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2acd953be44680d361f8",
+    "url": "/AF_Hackathon/static/js/runtime~main.c4a81cdb.js"
+  },
+  {
+    "revision": "8248b231edd569b571eb",
+    "url": "/AF_Hackathon/static/js/main.1c6e08bb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/AF_Hackathon/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8248b231edd569b571eb",
+    "url": "/AF_Hackathon/static/css/main.92d46904.chunk.css"
+  },
+  {
+    "revision": "8765b393ae147de110cac02bb8e904a8",
+    "url": "/AF_Hackathon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sahanwellala/AF_Hackathon.git/precache-manifest.49bd8104bac7ad73233c316920a2e25c.js"
+  "/AF_Hackathon/precache-manifest.183545f9d57bfb14c8ee2fa90906ef80.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sahanwellala/AF_Hackathon.git/index.html", {
+workbox.routing.registerNavigationRoute("/AF_Hackathon/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
